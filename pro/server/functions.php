ed885d6065d3533dbca3a74265f9a4ade0171ce0
Aazka <44495305+Aazka@users.noreply.github.com>
2019-01-23 12:15:01 +0500

    done with cookies and session task
    done with cookies and session task

--- pro/server/functions.php
+++ pro/server/functions.php
@@ -1,24 +1,6 @@
 <?php
 require_once "db_connection.php";
-if(isset($_POST['insert_pro']))
-{
-    // global $con;
-    $pro_title= $_POST['pro_title'];
-    $pro_cat=$_POST['pro_cat'];
-    $pro_brand=$_POST['pro_brand'];
-    $pro_price=$_POST['pro_price'];
-    $pro_desc=$_POST['pro_desc'];
-    $pro_keyword=$_POST['pro_kw'];
 
-    $insertQ="insert into product(pro_title,pro_cat,pro_brand,pro_price,pro_desc,pro_keyords)
-    values('$pro_title','$pro_cat','$pro_brand','$pro_price','$pro_desc','$pro_keyword');";
-    $result=mysqli_query($con,$insertQ);
-
-    if(!$result)
-    {
-        echo"not execute";
-    }
-}
 function getCats(){
     global $con;
     $getCatsQuery = "select * from categories";
@@ -26,7 +8,7 @@ function getCats(){
     while($row = mysqli_fetch_assoc($getCatsResult)){
         $cat_id = $row['cat_id'];
         $cat_title = $row['cat_title'];
-        echo "<option value='$cat_id'>$cat_title</option>";
+        echo "<li><a class='nav-link'  href='index.php?cat=$cat_id'>$cat_title</a></li>";
     }
 }
 function getBrands(){
@@ -36,20 +18,50 @@ function getBrands(){
     while($row = mysqli_fetch_assoc($getBrandsResult)){
         $brand_id = $row['brand_id'];
         $brand_title = $row['brand_title'];
-        echo "<option value='$brand_id'>$brand_title</option>";
+        echo "<li><a class='nav-link'  href='index.php?brand=$brand_id'>$brand_title</a></li>";
     }
 }
-function display_item()
-{
+
+function getPro(){
     global $con;
-    $getres="select * from (categories join product on cat_id=product.pro_cat) join brands on product.pro_brand=brands.brand_id";//"select * from product";
-    $getConnection=mysqli_query($con,$getres);
-    while($row = mysqli_fetch_assoc( $getConnection)){
-   // $row = mysqli_fetch_assoc( $getConnection);
-        $brand_id = $row['pro_id'];
-        $PRO_title = $row['pro_title'];
-        $PRO_category=$row['cat_title'];
-        $PRO_Btitle=$row['brand_title'];
-        echo $PRO_title,' ',$PRO_Btitle,' ',$PRO_category;//"<option value='$brand_id'>$brand_title</option>";
+    $getProQuery = '';
+    if(!isset($_GET['cat']) && !isset($_GET['brand']) && !isset($_GET['search'])){
+        $getProQuery = "select * from products order by RAND();";
+    }
+    else if(isset($_GET['cat'])){
+        $pro_cat_id = $_GET['cat'];
+        $getProQuery = "select * from products where pro_cat = '$pro_cat_id'";
+    }
+    else if(isset($_GET['brand'])){
+        $pro_brand_id = $_GET['brand'];
+        $getProQuery = "select * from products where pro_brand = '$pro_brand_id'";
+    }
+    else if(isset($_GET['search'])){
+        $user_query = $_GET['search'];
+        $getProQuery = "select * from products where pro_keywords like '%$user_query%'";
+    }
+    $getProResult = mysqli_query($con,$getProQuery);
+    $count_pro = mysqli_num_rows($getProResult);
+    if($count_pro==0){
+        echo "<h4 class='alert-warning align-center my-2 p-2'> No Product found in selected criteria </h4>";
+    }
+    while($row = mysqli_fetch_assoc($getProResult)){
+        $pro_id = $row['pro_id'];
+        $pro_title = $row['pro_title'];
+        $pro_price = $row['pro_price'];
+        $pro_image = $row['pro_image'];
+        echo "
+                <div class='col-sm-6 col-md-4 col-lg-3 text-center product-summary'>
+                    <h5 class='text-capitalize'>$pro_title</h5>
+                    <img src='admin/product_images/$pro_image'>
+                    <p> <b> Rs $pro_price/-  </b> </p>
+                    <a href='detail.php' class='btn btn-info btn-sm'>Details</a>
+                    <a href='#'>
+                        <button class='btn btn-primary btn-sm'>
+                            <i class='fas fa-cart-plus'></i> Add to Cart
+                        </button>
+                    </a>
+                </div>
+        ";
     }
 }
